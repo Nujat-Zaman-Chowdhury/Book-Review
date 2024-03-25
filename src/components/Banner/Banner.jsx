29796c8ed37c000f0cb89e0572364f96841b9d1b
Nujat-Zaman-Chowdhury <nujattazin@gmail.com>
2024-03-25 20:29:02 +0600

    added banner and navbar

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
         <div>
           <h1 className="text-5xl font-bold font-playfair leading-relaxed text-[#131313]">Books to freshen up 
           <br />your bookshelf</h1>
-          <Link to="/listed-books" className="btn mt-11 font-workSans bg-[#23BE0A] border-0 text-white">View The List</Link>
+          <Link to="/listed-books" target="_blank" className="btn mt-11 font-workSans bg-[#23BE0A] border-0 text-white">View The List</Link>
         </div>
       </div>
     </div>
